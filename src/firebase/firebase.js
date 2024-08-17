b53a8a1a9c808893428bddfdbd49bb9ba64d5eca
labsaurus <116987438+labsaurus@users.noreply.github.com>
2024-08-17 22:05:18 +0700

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyANO8XXuxgvXXBajnzggg9YObKxdoWuRGI",
-  authDomain: "parascore-a1be3.firebaseapp.com",
-  projectId: "parascore-a1be3",
-  storageBucket: "parascore-a1be3.appspot.com",
-  messagingSenderId: "753371744593",
-  appId: "1:753371744593:web:885ed6f339a723e79dab70",
-  measurementId: "G-LPEEEP6G4E"
+  apiKey: "AIzaSyAPKzHcw5H2hccLbf7lzzg_q-YydVyQLQA",
+  authDomain: "metaldetect-59625.firebaseapp.com",
+  projectId: "metaldetect-59625",
+  storageBucket: "metaldetect-59625.appspot.com",
+  messagingSenderId: "128603243572",
+  appId: "1:128603243572:web:a4649b4330369f26c8f661",
+  measurementId: "G-JKXJWHF9WN"
 };
 
 // Initialize Firebase
